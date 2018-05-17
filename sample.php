dac14153ea621155a231ead09b4d65415faa17af
Kai Hasegawa <kai.guysecil.gist@gmail.com>
2018-05-17 23:01:11 +0900

    Add printArray()
    and sample data

--- sample.php
+++ sample.php
@@ -1,3 +1,12 @@
 <?php
     echo "Hello";
+
+    function printArray(array $arr) {
+      for ($i = 0; $i < count($arr); $i++) {
+        echo $arr[$i] . " ";
+      }
+      echo "<br>";
+    }
+
+    $arr = [1, 2, 3];
 ?>
