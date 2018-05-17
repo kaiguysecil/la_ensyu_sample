1230c7da53ce0eaa8536da1cdb7b43e8dc50f75f
Kai Hasegawa <kai.guysecil.gist@gmail.com>
2018-05-17 23:03:22 +0900

    Add printArrayEach
    and sample data

--- sample.php
+++ sample.php
@@ -1,3 +1,13 @@
 <?php
     echo "Hello";
+
+    function printArrayEach(array $arr) {
+        foreach ($arr as $value) {
+            echo $value . " ";
+        }
+        echo "<br>";
+    }
+
+    $arr2 = [4, 5, 6];
+    printArrayEach($arr2);
 ?>
