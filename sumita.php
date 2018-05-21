f99ade15570b197a30435148ad1be82c949d1333
SUMITA satoru <s.sumita@ids.co.jp>
2018-05-21 14:16:18 +0900

    create class

---
+++ sumita.php
@@ -0,0 +1,12 @@
+<?php
+ class Sumita{
+    public $age;
+    public $name;
+    public $adress;
+ }
+
+ $sumita = new Sumita;
+ $sumita->$age = 2000;
+ $sumita->$name = "satoru";
+ $sumita->$adress = "Saitama";
+?>
